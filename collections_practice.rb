eb0897e6f8645fe444f3018db97ca78a96852060
Elisa Vaccaro <erv230@nyu.edu>
2017-10-06 15:39:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
-# your code goes here
+def begins_with_r(array)
+  holder = 0
+  array.each do |word|
+    if word.start_with?("r")
+      holder += 1
+    end
+  end
+    if holder == array.length
+      return true
+    else
+      return false
+    end
+end
+
+
+def contain_a(array)
+  answer = []
+  array.each do |word|
+    if word.include?"a"
+      answer.push(word)
+    end
+  end
+  answer
+end
+
+def first_wa(array)
+  array.each do |word|
+    if word.to_s.start_with?("wa")
+      return word
+    end
+  end
+end
+
+def remove_non_strings(array)
+  answer = []
+  array.each do |item|
+    if item.class == String
+      answer.push(item)
+    end
+  end
+  answer
+end
